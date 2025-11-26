c153d5255b45174aed9309ce0ba6d56b5f2d66c0
Krzysztof Rymski <krymski@google.com>
2025-11-26 01:05:06 -0800

    Internal changes
    PiperOrigin-RevId: 837001762

---
+++ gemma/kv_cache_test.cc
@@ -0,0 +1,11 @@
+#include "gemma/kv_cache.h"
+
+#include "gtest/gtest.h"
+#include "gemma/configs.h"
+#include "gemma/gemma_args.h"
+#include "util/threading_context.h"
+namespace gcpp {
+namespace {
+
+}  // namespace
+}  // namespace gcpp
